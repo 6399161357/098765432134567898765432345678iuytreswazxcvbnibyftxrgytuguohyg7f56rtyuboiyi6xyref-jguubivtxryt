d2d477113f7fe516ce8a88d2224829a9662fadcd
6399161357 <jay990x@gmail.com>
2026-06-17 14:16:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = (
         f"<b>This bot is made for {NAME2}</b>\n\n"
         f"<i>Powered by</i> <a href='https://t.me/Light_speedy'>L ɪ ɢ ʜ ᴛ</a>\n"
+         f"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
     )
     
     keyboard = [
